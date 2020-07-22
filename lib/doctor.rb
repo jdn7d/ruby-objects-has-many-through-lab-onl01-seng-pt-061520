77839593ae4f03b0dba0c80a4847fdb89759bc56
Jessie Donovan <donovanajessie@gmail.com>
2020-07-22 01:43:19 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,7 +16,7 @@ class Doctor
     Appointment.new(date, patient, self)
   end
 
-  def appointments
+  def appointments(date, patient)
     Appointment.all.select { |appointment| appointment.doctor == self }
   end
 
